443b5da26625551e65c50e85a89a91676d114a5d
chanhwi <gazebo5@naver.com>
2022-12-09 13:20:06 +0900

    결과창 애니메이션 추가

--- src/components/ResultPopup.jsx
+++ src/components/ResultPopup.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import ScoreBar from './ScoreBar';
 
 function ResultPopup({ show, setShow, tries, scores, setScores, gameCount, gameOver, answerWord }) {
-  if (!show) return null;
   const winCount = Object.entries(scores).reduce((p, [a, b]) => p + b, 0);
   const maxCount = Object.entries(scores).reduce((p, [a, b]) => Math.max(p, b), 0);
   const winRatio = Math.floor(winCount / gameCount * 100);
 
   return (
-    <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center',
+                  opacity: `${show ? '100' : '0'}`, zIndex: `${show ? 1 : -1}`, transition: 'all', transitionDuration: '0.5s' }}>
         <div style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5' }}></div>
         <div style={{ position: 'relative', width: '500px', backgroundColor: 'white', display: 'flex', flexDirection: 'column', 
-                      justifyContent: 'center', alignItems: 'center', borderRadius: '8px', zIndex: 1, padding: '8px' }}>
+                      justifyContent: 'center', alignItems: 'center', borderRadius: '8px', zIndex: 1, padding: '8px',
+                      transition: 'all', transitionDuration: '0.5s', marginTop: `${show ? '0%' : '5%'}` }}>
           <header>
             <h1>Game Result</h1>
             <button style={{ position: 'absolute', top: 8, right: 8, width: '32px', height: '32px', outline: 'none', border: 'none',
@@ -37,7 +38,7 @@ function ResultPopup({ show, setShow, tries, scores, setScores, gameCount, gameO
               {Object.entries(scores).map(([score, count]) => {
                 return <div style={{ display: 'flex', width: '80%' }} key={score}>
                   <div style={{ margin: 0, fontWeight: 'bold', marginRight: '4px' }}>{score}</div>
-                  <ScoreBar active={gameOver === 1 && tries === parseInt(score)} count={count} ratio={!maxCount ? 100 : Math.floor(count / maxCount * 100)} />
+                  <ScoreBar active={gameOver === 1 && tries === parseInt(score)} count={count} ratio={!maxCount ? 100 : Math.max(10, Math.floor(count / maxCount * 100))} />
                 </div>
               })}
             </div>
@@ -46,8 +47,8 @@ function ResultPopup({ show, setShow, tries, scores, setScores, gameCount, gameO
                            width: '100%', marginTop: '16px', marginBottom: '40px  ' }}>
             <h3>Win VS Lose</h3>
             <div style={{ width: '80%', backgroundColor: 'black', display: 'flex', color: 'white', fontWeight: 'bold' }}>
-              <div style={{ width: `${winRatio}%`, padding: '12px 0', backgroundColor: '#5CB85C', display: 'flex', justifyContent: 'center' }}>{winCount > 0 && winCount}</div>
-              <div style={{ width: `${100 - winRatio}%`, padding: '12px 0', backgroundColor: '#ff4f4f', display: 'flex', justifyContent: 'center' }}>{gameCount > winCount && gameCount - winCount}</div>
+              <div className='slide-expand' style={{ width: `${winRatio}%`, padding: '12px 0', backgroundColor: '#5CB85C', display: 'flex', justifyContent: 'center' }}>{winCount > 0 && winCount}</div>
+              <div className='slide-expand' style={{ width: `${100 - winRatio}%`, padding: '12px 0', backgroundColor: '#ff4f4f', display: 'flex', justifyContent: 'center' }}>{gameCount > winCount && gameCount - winCount}</div>
             </div>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '80%' }}>
               <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 function ScoreBar({ count, ratio, active }) {
   return (
-    <div style={{ width: `${ratio}%`, minWidth:'10%', display: 'flex', fontWeight: 'bold',
-                  justifyContent: 'center', alignItems: 'center', color: 'white', backgroundColor: `${active ? '#5CB85C' : '#787878'}`,
-                }}>
+    <div className='slide-expand' style={{ width: `${ratio}%`, display: 'flex', fontWeight: 'bold',
+                  justifyContent: 'center', alignItems: 'center', color: 'white', backgroundColor: `${active ? '#5CB85C' : '#787878'}`}}>
         {count}
     </div>
   )
 }
 
-export default ScoreBar
+export default ScoreBar;
